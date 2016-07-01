9bea7d7b38116e1f476ab72c88aca40d8ff0dbc5
Brian Dorton <dorton@gmail.com>
2016-06-30 19:55:10 -0500

    gets application theme up.  tons of extra files.  no working navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require moment
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
-//= require twitter/bootstrap
-//= require moment
 //= require bootstrap-datetimepicker
 //= require turbolinks
-//= require_tree .
+//= require template
+//= require modernizr
+//= require jquery.magnific-popup.min
+//= require jquery.waypoints.min
+//= require jquery.countTo
+//= require jquery.parallax-1.1.3
+//= require jquery.validate
+//= require owl.carousel
+//= require jquery.browser
+//= require SmoothScroll
+//= require jquery.plugin
